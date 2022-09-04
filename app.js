22c972337bee4bcc72ad718d88d77aeb42275038
ibrahim364 <you@example.com>
2022-09-04 21:31:16 +0100

    fix: more lint

--- app.js
+++ app.js
@@ -1,35 +1,35 @@
-import express from "express";
-import rateLimiter from "./src/middlewares/rateLimitter.js";
-import howoldRoute from "./src/routes/howOldRoute.js";
-import cors from "cors";
+import express from 'express'
+import rateLimiter from './src/middlewares/rateLimitter.js'
+import howoldRoute from './src/routes/howOldRoute.js'
+import cors from 'cors'
 
-const app = express();
+const app = express()
 
-app.use(cors());
+app.use(cors())
 
-app.use(rateLimiter);
+app.use(rateLimiter)
 
-app.use('/', howoldRoute);
+app.use('/', howoldRoute)
 
 // error handler
 app.use(function (err, _req, res, _next) {
-    if (err.httpStatusCode) {
-        res.status(err.httpStatusCode)
-            .json({
-                success: false,
-                message: err.message || 'Something went wrong, it\`ll be nice if you report this to us.',
-                status: err.httpStatusCode,
-                data: err.data || {}
-            });
-    } else {
-        res.status(500)
-            .json({
-                success: false,
-                message: 'Something went wrong, it\`ll be nice if you report this to us.',
-                status: 500,
-                data: {}
-            });
-    }
-});
+  if (err.httpStatusCode) {
+    res.status(err.httpStatusCode)
+      .json({
+        success: false,
+        message: err.message || 'Something went wrong, it`ll be nice if you report this to us.',
+        status: err.httpStatusCode,
+        data: err.data || {}
+      })
+  } else {
+    res.status(500)
+      .json({
+        success: false,
+        message: 'Something went wrong, it`ll be nice if you report this to us.',
+        status: 500,
+        data: {}
+      })
+  }
+})
 
 export default app
