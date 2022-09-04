7c88e965c1a697242b091be2ac45c01d0a42072f
ibrahim364 <you@example.com>
2022-09-04 23:20:13 +0100

    .

--- src/services/howOldService.js
+++ src/services/howOldService.js
@@ -1,15 +1,11 @@
-import { ValidationError } from '../helpers/errors.js'
-
 export const howOldService = async (query) => {
   if (!query.dob) {
-    return 'nosdf'
-    //throw new ValidationError('No date of birth passed to request')
+    return 'Invalid date'
   }
 
   const dateOfBirth = new Date(query.dob)
 
   if (dateOfBirth.toString() === 'Invalid Date') {
-    //throw new ValidationError('Invalid date')
     return 'Invalid date'
   }
   return calculateAge(dateOfBirth)
@@ -19,9 +15,4 @@ function calculateAge (dateString) {
   const ageDifMs = Date.now() - new Date(dateString).getTime()
   const ageDate = new Date(ageDifMs)
   return Math.abs(ageDate.getUTCFullYear() - 1970)
-  // const ageInMilliseconds = new Date() - dateString
-
-  // // convert to years
-  // const age = Math.floor(ageInMilliseconds / 1000 / 60 / 60 / 24 / 365)
-  // return age
 }
