f5034c326b0bfaacbfabd0cdef7a7c4c6dabf1d8
ibrahim364 <you@example.com>
2022-09-04 16:02:30 +0100

    .

--- src/middlewares/rateLimitter.js
+++ src/middlewares/rateLimitter.js
@@ -46,8 +46,8 @@ const rateLimiter = (req, res, next) => {
                 };
                 // if last request is still within the window but the number of request is greater than the time limit, throw an error
             } else {
-                res.status(429).send(`You have exceeded the ${MAX_REQUEST_PER_WINDOW} requests limit in ${WINDOW_SIZE_IN_SECONDS} seconds limit!`)
-                //throw new RequestLimitError(`You have exceeded the ${MAX_REQUEST_PER_WINDOW} requests limit in ${WINDOW_SIZE_IN_SECONDS} seconds limit!`)
+                //res.status(429).send(`You have exceeded the ${MAX_REQUEST_PER_WINDOW} requests limit in ${WINDOW_SIZE_IN_SECONDS} seconds limit!`)
+                throw new RequestLimitError(`You have exceeded the ${MAX_REQUEST_PER_WINDOW} requests limit in ${WINDOW_SIZE_IN_SECONDS} seconds limit!`)
             }
 
             next()
