bec2aa82a55ba14d23965eb93643db32af7701e6
ibrahim364 <you@example.com>
2022-09-05 12:12:58 +0100

    fix: removed error messsags

--- src/controllers/howOldController.js
+++ src/controllers/howOldController.js
@@ -2,17 +2,13 @@ import { howOldService } from '../services/howOldService.js'
 
 export const howOldController = async (req, res, next) => {
   try {
-    const response = await howOldService(req.query)
-    if (response !== 'Invalid input') {
-      res.status(200).json({
-        success: true,
-        message: 'Age calculated successfully',
-        status: 200,
-        data: { age: response }
-      })
-    } else {
-      res.status(400).json('Invalid input')
-    }
+    const age = await howOldService(req.query)
+    res.status(200).json({
+      success: true,
+      message: 'Age calculated successfully',
+      status: 200,
+      data: { age }
+    })
   } catch (error) {
     next(error)
   }
