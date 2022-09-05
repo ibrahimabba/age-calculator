ef0a33388eba50b38c6f5207954059aab9176bf8
ibrahim364 <you@example.com>
2022-09-05 12:54:15 +0100

    refactor:

--- src/controllers/howOldController.js
+++ src/controllers/howOldController.js
@@ -3,11 +3,11 @@ import { howOldService } from '../services/howOldService.js'
 export const howOldController = async (req, res, next) => {
   try {
     const age = await howOldService(req.query)
-    res.status(200).json({
-      success: true,
-      message: 'Age calculated successfully',
-      status: 200,
-      data: { age }
+    res.json({
+      success: typeof age === 'number',
+      message: typeof age === 'number' ? 'Age calculated successfully' : 'Invalid input',
+      status: typeof age === 'number' ? 200 : 400,
+      data: { ...(typeof age === 'number' && { age }) }
     })
   } catch (error) {
     next(error)
